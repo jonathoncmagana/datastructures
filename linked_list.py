a960fd003d63b9f7909232d20fd53d99b6e04a7e
magana <jonathoncmagana@gmail.com>
2023-07-19 19:11:32 -0500

    TODO for THurs: see TODO comments,  code remove

--- linked_list.py
+++ linked_list.py
@@ -7,6 +7,8 @@ class Node(object):  # One part of the data Linked List
         self.data = data  # The data object for this Node
         self.next = None  # Reference to next Node
 
+    def __str__(self):
+        return str(self.data)
 
 
 class LinkedList(object):  # A linked list of data elements
@@ -28,30 +30,59 @@ class LinkedList(object):  # A linked list of data elements
     # 1. assume we have a valid Linked List that we are adding to
     # 2. make sure all linked list parameters are updated in the function
     # 3. at end of function, make sure we have a valid linked
+
+    # what's big O time add itself is O(n) (linear time)
+    # TODO can re-write add, and other functions, so that only data comes in, not a Node
+    # TODO Can re-write add so it DOESn't have a loop?
     def add(self, other: Node): #add to end
         cur = self.__head
-
+        steps = 1  # we count 1 step for coming in a function at all
         if (self.size ==0): # empty list coming in to add
             self.__head = other
         else:
             while (cur.next is not None):
                     cur = cur.next
+                    steps += 1  # we could each time a loop runs (based on "n" - the number of things in the list)
             cur.next = other
         self.size += 1 # we added one Node
+        # print (f"steps for add: {steps}, related to \"n\", the number of items in the list")
 
 
-    # TODO before Weds: code get
+    # big O of get? Assume worse case scenario! O(n) linear
     def get(self, index:int): # where head is index 0, head->next would be index 1, etc.
-        pass
+        # we could improve this code by checking for a valid index
+        cur = self.__head
+        steps = 1
+        for i in range(index):
+            cur = cur.next
+            steps += 1
+        # print(f"steps for get: {steps}, related to index we are getting")
+        return cur
         # return a Node at index
 
-    # TODO before Weds: code or psuedocode
+
     def addAt(self, index: int, other:Node):
-        pass
+
+        # what if we addAt(0)?
+        # TODO we need to update the __head!!!!
+
+        # it is inefficient to call get twice, can we only call it once?
+        nodeiminus1 = self.get(index-1) # big O(n)
+        nodei = self.get(index) # big O(n)
+        # TODO how to re-write to call get only once
+
+        # big O(2n), but so what, this is just O(n)
+
+        # we identified 2 operations we needed from our picture:
+        nodeiminus1.next = other
+        other.next = nodei
+
     # remove
 
-    # TODO before Weds: code or psuedocode
+    # TODO before Thurs: code
     def remove(self, index:int):
+        # see picture from video
+        # (i-1).next = (i.next)
         pass
     # print
     # assmume we have a valid list coming in
@@ -63,7 +94,7 @@ class LinkedList(object):  # A linked list of data elements
             return "empty linked list"
         else:  # we don't have an empty list
             while (cur.next is not None):
-                message += cur.data + " -> " # add the current node's data on to the message
+                message += f"{str(cur.data)} -> " # add the current node's data on to the message
                 cur = cur.next
-            message += cur.data # add the last node's data to the message
+            message += str(cur.data)  # add the last node's data to the message
         return message
--- list_driver.py
+++ list_driver.py
@@ -1,22 +1,53 @@
 from linked_list import LinkedList, Node
+from timeit import default_timer as timer
 def main():
     print("Linked List driver!")
 
     my_linked_list = LinkedList() # creates an empty linked list, size 0, and head points to None
+    """
+    #my_linked_list.add(Node("first"))
+    for i in range(10):
+        my_linked_list.add(Node(i))
 
-    my_linked_list.add(Node("first"))
-
-    my_linked_list.add(Node("second"))
-
+    my_linked_list.add(Node("hello mom"))
     print(my_linked_list)
+
+    my_linked_list.addAt(4, Node("I am the new four"))
+    print(my_linked_list)
+    print(f"test out get(4): {my_linked_list.get(8)}")
+
+    # my_linked_list2 = LinkedList()
+    # print(f"test out get(0): {my_linked_list2.get(0)}")
+
+
+    
+    start = timer()
+    # we are timing the code between start and end
+    my_linked_list.add(Node("second"))
+    end = timer()
+    print(f"time to add \"second\": {end - start}")  # Time in seconds, e.g. 5.38091952400282
 
     my_linked_list.add(Node("third"))
 
-    print(my_linked_list)
+    # print(my_linked_list)
 
+    """
+    start = timer()
+    # we are timing the code between start and end
+    for i in range(200): # when you double this number, the time more than doubles. why? O(n^2)
+        my_linked_list.add(Node(i))
+    end = timer()
+    print(f"time to add numbers: {end - start} seconds")  # Time in seconds, e.g. 5.38091952400282
+    # my_linked_list.get(89)
+    # print(my_linked_list)
 
-
-
+    steps = 1
+    for i in range(200):  # heres an n
+        for j in range(200): # heres an n
+            for k in range(200): # heres an n
+                steps += 1
+    # the whole thing is O(n^3)
+    print (steps)
 
 if __name__ == "__main__":
     main()
