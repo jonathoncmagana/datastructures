83f4377d9f91ae4fb9fba6950ea15ba7a87ef9a8
magana <jonathoncmagana@gmail.com>
2023-07-24 19:03:09 -0500

    Ex for Weds: code find, pseudo code for __str__

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -2,10 +2,9 @@
 # for compatibility with older versions of python).
 # It used to be needed in python 2,
 # but in python 3 it is no longer needed
-class Node(object):  # One part of the data Linked List
+class Node(object):  # One part of the data Tree
     def __init__(self, data):  # Constructor
         self.data = data  # The data object for this Node
-        self.next = None  # Reference to next Node
         self.left = None  # the left child
         self.right = None  # the right child
 
@@ -22,39 +21,46 @@ class BST(object):  # A binary search tree of data elements
 
     # add (aka insert)
     # this function does NOT keep the tree balanced
-    def add(self, dataToAdd):
-        nodeToAdd = Node(dataToAdd)
-        cur = self.__root
+    # Rules for data struct functions:
+    # 1. before function is called, we have a valid data structure (BST)
+    # 2. update all attributes (root, size, left and right of Nodes)
+    # 3. on the way out, the function must leave the data structure valid
+    def add(self, dataToAdd): # we decided that adding is log(N) (LOG BASE 2)
+        nodeToAdd = Node(dataToAdd) # QoL improvement, hide Nodes from user
+        cur = self.__root  # the current Node we are looking at
 
         self.size += 1
-
+        steps = 1
         if cur is None:  # For empty trees, insert new node at root
             self.__root = nodeToAdd  # root of tree
         else:
-
+            # we at least have a root already
             lookingForSpot = True
 
             while (lookingForSpot):
+                steps +=1
                 if dataToAdd < cur.data:  # If new data is less cur's data, go left
-                    if (cur.left is None):
+                    if (cur.left is None):  # if there is a open spot
                         cur.left = nodeToAdd
                         lookingForSpot = False
                     else:
-                        cur = cur.left
+                        cur = cur.left  # equivalent of cur = cur.next in LL. Meaning keeping going
                 else:  # Otherwise >= look right
                     if (cur.right is None):
                         cur.right = nodeToAdd
                         lookingForSpot = False
                     else:
-                        cur = cur.right
+                        cur = cur.right  # equivalent of cur = cur.next in LL. Meaning keeping going
+            print (f"steps for adding {dataToAdd}: {steps}")
 
 
-    # TODO find
+    # TODO find. Excersize for Weds, try to code. return True if it finds it, false if not
     def find(self, dataToFind): # takes data, NOT a Node
         pass
 
     # TODO print with __str__ is "hard coded", needs to be better
     # TODO can we make it generic, and then, can we use recursion?
+    # TODO think about psuedo code for more generic printing. Doesn't have to be pretty
     def __str__(self):
         message = f"\t  {self.__root}\n"
 
--- tree_driver.py
+++ tree_driver.py
@@ -3,16 +3,25 @@ from timeit import default_timer as timer
 def main():
     print("BST driver!")
 
+    mywords = BST()
+
+    mywords.add("mom")
+    mywords.add("sister")
+    mywords.add("dad")
+
+
+    #print(mywords)
+
+
     my_BST = BST() # creates an empty BST, size 0, and root points to None
 
     # TODO: Question - the order I add things in matters to how the tree
     # TODO gets structured. Why?
     my_BST.add(4)
-    my_BST.add(2)
-    my_BST.add(3)
-    my_BST.add(1)
-
     my_BST.add(6)
+    my_BST.add(2)
+    my_BST.add(1)
+    my_BST.add(3)
     my_BST.add(5)
     my_BST.add(7)
 
